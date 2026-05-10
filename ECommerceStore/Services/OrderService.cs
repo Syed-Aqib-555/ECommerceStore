23608180aa8f4781d0a2150ae8f3feb5a3b21d93
Syed-Aqib-555 <syedaqibh555@gmail.com>
2026-05-10 23:49:30 +0500

    Implement OrderService to manage selected products and order processing

---
+++ ECommerceStore/Services/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceStore.Models;
+
+namespace ECommerceStore.Services
+{
+    public class OrderService
+    {
+        private Product? selectedProduct;
+        private Order? lastOrder;
+
+        public void SetSelectedProduct(Product product)
+        {
+            selectedProduct = product;
+        }
+
+        public Product? GetSelectedProduct()
+        {
+            return selectedProduct;
+        }
+
+        public void PlaceOrder(Order order)
+        {
+            lastOrder = order;
+        }
+
+        public Order? GetLastOrder()
+        {
+            return lastOrder;
+        }
+    }
+}
